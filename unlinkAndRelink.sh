ac87fd14a0912619d54a9eff46950244baa417cc
ioanlongjing <ioan.snickersnee@gmail.com>
2016-10-28 22:25:16 +0800

    加一點小修改註解

--- unlinkAndRelink.sh
+++ unlinkAndRelink.sh
@@ -1,6 +1,6 @@
 # 先確認斷掉所有 link
+# 在重新link 軟體, 不行的話可以考慮brwe link --force [軟體] 
 ls -1 /usr/local/Library/LinkedKegs | while read line; do echo $line; brew unlink $line; brew link --force $line; done
 
-# 重新連結所有link
 brew list -1 | while read line; do brew unlink $line; brew link $line; done
 
